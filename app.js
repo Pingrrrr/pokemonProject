618112d4ea912ab9f88eb17182401c2ab6755183
Laura Boyle <156213687+Pingrrrr@users.noreply.github.com>
2024-04-18 12:54:52 +0100

    render the landing page with ejs template, added gitignore

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PORT = 3000;
 
 app.use(express.static('static'));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
     res.send("<h2>My Express Web App </h2>")
@@ -14,4 +15,13 @@ app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, '/static', 'tradecard.html'))
 });
 
+app.get("/landing", function (req, res){
+    res.render("tradecard");
+});
+
+app.get('*', (req, res) => {
+    res.send("404! Whoops, I cannot find that!");
+});
+
 app.listen(PORT, () => console.log("Listening on port 3000!"));
+
