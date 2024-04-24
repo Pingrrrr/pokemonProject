86df7cbb357ae90ab2939eb6fda26670f13f347c
Laura Boyle <156213687+Pingrrrr@users.noreply.github.com>
2024-04-24 18:25:39 +0100

    retrieved all card information from DB and displayed in card ejs

--- app.js
+++ app.js
@@ -46,26 +46,49 @@ app.get("/cards", function (req, res) {
 });
 
 //ONE CARD
-app.get('/card', (req, res) => {
+app.get('/card', async (req, res) => {
     const card_id = req.query.id;
     const readcard = `SELECT * FROM card WHERE card_id = ${card_id} `;
-    db.query(readcard, (err, dataset) => {
-        if (err) throw err;
-        let card = dataset[0];
-        const cardattack = `  SELECT attack.* FROM card
+    let cardSQL = await db.promise().query(readcard);
+    let card = cardSQL[0][0];
+
+    const readtype = `SELECT * FROM card_type INNER JOIN type ON card_type.type_id=type.type_id WHERE card_type.card_id=${card_id} `;
+    let typeSQL = await db.promise().query(readtype);
+    let types = typeSQL[0];
+    card.types=types;
+
+    const cardattack = `  SELECT attack.* FROM card
 	INNER JOIN card_attack ON card_attack.card_id = card.card_id
     INNER JOIN attack ON card_attack.attack_id = attack.attack_id
 WHERE card.card_id = ${card_id}`;
-        db.query(cardattack, (err, attacks) => {
-            if (err) throw err;
-            card.attacks=attacks;
-            console.log(card);
-            res.render('card', { card });
-        });
+    let attackSQL = await db.promise().query(cardattack);
+    let attacks = attackSQL[0];
+    card.attacks=attacks;
+
+    const cardabilties = `  SELECT ability.* FROM card
+	INNER JOIN card_ability ON card_ability.card_id = card.card_id
+    INNER JOIN ability ON card_ability.ability_id = ability.ability_id
+WHERE card.card_id = ${card_id}`;
+    let abilitySQL = await db.promise().query(cardabilties);
+    let abilities = abilitySQL[0];
+    card.abilities=abilities;
+
+    const cardresist = ` SELECT * FROM resistance INNER JOIN type ON resistance.type_id = type.type_id WHERE resistance.card_id = ${card_id}`;
+    let resistanceSQL = await db.promise().query(cardresist);
+    let cardresistances = resistanceSQL[0];
+    card.resistances=cardresistances;
+    
+
+    const cardweakness = ` SELECT * FROM weakness INNER JOIN type ON weakness.type_id = type.type_id WHERE weakness.card_id  = ${card_id}`;
+    let weaknessSQL = await db.promise().query(cardweakness);
+    let cardweaknesses = weaknessSQL[0];
+    card.weaknesses=cardweaknesses;
+    
 
 
+    console.log(card);
+    res.render('card', { card });
 
-    })
 });
 
 app.get('*', (req, res) => {
